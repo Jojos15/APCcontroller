33490dde141675dc9d20fd28d65b0bccbead5732
George Kosmidis <georgekosmidis1@yahoo.gr>
2018-06-05 00:38:54 +0300

    All buttons working. Leds for all except taps bv needed BPM

--- src/MainThread.java
+++ src/MainThread.java
@@ -11,6 +11,7 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
     private ArrayList<Tap> taps = new ArrayList<>();
     private ArrayList<Macro> macros = new ArrayList<>();
     private ArrayList<Fader> faders = new ArrayList<>();
+    private ArrayList<Integer> macroToRelease = new ArrayList<>();
     private int lastFaderCC = 0;
 
 
@@ -21,22 +22,21 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
 
     @Override
     public void init() throws DeviceNotFoundException {
-        for(int i=0;i<Variables.playbacks.length;i+=2){
-            playbacks.add(new Playback(Integer.parseInt(Variables.playbacks[i]), Variables.playbacks[i+1]));
+        for (int i = 0; i < Variables.playbacks.length; i += 2) {
+            playbacks.add(new Playback(Integer.parseInt(Variables.playbacks[i]), Variables.playbacks[i + 1]));
         }
-        for(int i=0;i<Variables.stops.length;i+=2){
-            stops.add(new Stop(Integer.parseInt(Variables.stops[i]), Variables.stops[i+1]));
+        for (int i = 0; i < Variables.stops.length; i += 2) {
+            stops.add(new Stop(Integer.parseInt(Variables.stops[i]), Variables.stops[i + 1]));
         }
-        for(int i=0;i<Variables.taps.length;i+=2){
-            taps.add(new Tap(Integer.parseInt(Variables.taps[i]), Variables.taps[i+1]));
+        for (int i = 0; i < Variables.taps.length; i += 2) {
+            taps.add(new Tap(Integer.parseInt(Variables.taps[i]), Variables.taps[i + 1]));
         }
-        for(int i=0;i<Variables.macros.length;i+=3){
+        for (int i = 0; i < Variables.macros.length; i += 3) {
             boolean flag = false;
-            if(Variables.macros[i+2].equals("TRUE")){
+            if (Variables.macros[i + 2].equals("TRUE")) {
                 flag = true;
-                getService().log(flag + "");
             }
-            macros.add(new Macro(Integer.parseInt(Variables.macros[i]), Variables.macros[i+1], flag));
+            macros.add(new Macro(Integer.parseInt(Variables.macros[i]), Variables.macros[i + 1], flag));
         }
     }
 
@@ -44,45 +44,91 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
     public boolean process(MessageWrapper messageWrapper, String s) throws Throwable {
         ShortMessageWrapper shortMessageWrapper = messageWrapper.getAsShortMessageWrapper();
         //CASE: NOTE ON
-        if(shortMessageWrapper.isNoteOn()) {
+        if (shortMessageWrapper.isNoteOn()) {
             //CASE 2 ON KEYS EXCEPTION
             if (shortMessageWrapper.getData1() != 88 && shortMessageWrapper.getData1() != 89) {
                 //FIND IN WHICH CATEGORY IT IS
-                if (shortMessageWrapper.getData1()<=7){ // STOP
-                    Keyboard keyboard = new Keyboard();
-                    keyboard.type(stops.get((shortMessageWrapper.getData1()+Variables.stops_offset)%10).getVirtualKey());
-                }
-                else if(shortMessageWrapper.getData1()<=15){ // TAP
-                    Keyboard keyboard = new Keyboard();
-                    keyboard.type(taps.get((shortMessageWrapper.getData1()+Variables.taps_offset)%10).getVirtualKey());
-                }
-                else if(shortMessageWrapper.getData1()<=63&&shortMessageWrapper.getData1()>=56){ // MACRO
-                    Keyboard keyboard = new Keyboard();
-                    keyboard.type(macros.get((shortMessageWrapper.getData1()+Variables.macros_offset)%10).getVirtualKey());
-                }
-                else if(shortMessageWrapper.getData1()<=71&&shortMessageWrapper.getData1()>=64) { // PLAYBACK
-                    Playback p = playbacks.get((shortMessageWrapper.getData1()+Variables.playbacks_offset)%10);// NOT WORKING
+                if (shortMessageWrapper.getData1() <= 7) { // STOP
+                    int position = (shortMessageWrapper.getData1() + Variables.stops_offset) % 10;
 
                     Keyboard keyboard = new Keyboard();
-                    keyboard.type(p.getVirtualKey());
+                    keyboard.type(stops.get(position).getVirtualKey());
 
                     //SET LEDS
-                    p.setOn(!p.isOn());
-                    setLed(p.isOn(), shortMessageWrapper, Variables.RED, Variables.OFF);
-                }
-            }
-            else{
-                //DOESN'T WORK CHANGE TO FOR
-               for(Playback p : playbacks){
-                    if(p.getCc() == shortMessageWrapper.getData1()){
-                        Keyboard keyboard = new Keyboard();
-                        keyboard.type(p.getVirtualKey());
-
-                        p.setOn(!p.isOn());
-                        setLed(p.isOn(), shortMessageWrapper, Variables.GREEN, Variables.OFF);
-                        break;
+                    //TODO: ONLY FOR MY APC
+                    if(shortMessageWrapper.getData1()==6||shortMessageWrapper.getData1()==7){
+                        setLed(stops.get(position).isOn(), shortMessageWrapper, Variables.GREEN, Variables.OFF);
                     }
+                    setLed(stops.get(position).isOn(), shortMessageWrapper, Variables.YELLOW, Variables.OFF);
+
+
+                } else if (shortMessageWrapper.getData1() <= 15) { // TAP
+                    Keyboard keyboard = new Keyboard();
+                    keyboard.type(taps.get((shortMessageWrapper.getData1() + Variables.taps_offset) % 10).getVirtualKey());
+                } else if (shortMessageWrapper.getData1() <= 63 && shortMessageWrapper.getData1() >= 56) { // MACRO
+                    int position = (shortMessageWrapper.getData1() + Variables.macros_offset) % 10;
+
+                    Keyboard keyboard = new Keyboard();
+                    keyboard.type(macros.get(position).getVirtualKey());
+
+                    //SET LEDS
+                    macros.get(position).setOn(!macros.get(position).isOn());
+                    setLed(macros.get(position).isOn(), shortMessageWrapper, Variables.RED, Variables.OFF);
+
+                    if(macros.get(position).isHold()){
+                        macroToRelease.add(shortMessageWrapper.getData1());
+                    }
+
+                } else if (shortMessageWrapper.getData1() <= 71 && shortMessageWrapper.getData1() >= 64) { // PLAYBACK
+                    int position = (shortMessageWrapper.getData1() + Variables.playbacks_offset) % 10;
+
+                    Keyboard keyboard = new Keyboard();
+                    keyboard.type(playbacks.get(position).getVirtualKey());
+
+                    //SET LEDS
+                    playbacks.get(position).setOn(!playbacks.get(position).isOn());
+                    setLed(playbacks.get(position).isOn(), shortMessageWrapper, Variables.RED, Variables.OFF);
+                    if(!playbacks.get(position).isOn()){
+                        stops.get(position).setOn(false);
+                        ShortMessageWrapper messageWrapper1 = shortMessageWrapper;
+                        messageWrapper1.alterData1(stops.get(position).getCc());
+                        setLed(stops.get(position).isOn(), messageWrapper1, 0, Variables.OFF);
+                    }
+                    else stops.get(position).setOn(true);
                 }
+            } else {
+                //TODO: ONLY FOR MY APC
+                int position = 9;
+                if (shortMessageWrapper.getData1() == 88) position = 8;
+                Keyboard keyboard = new Keyboard();
+                keyboard.type(playbacks.get(position).getVirtualKey());
+
+                //SET LEDS
+                playbacks.get(position).setOn(!playbacks.get(position).isOn());
+                setLed(playbacks.get(position).isOn(), shortMessageWrapper, Variables.GREEN, Variables.OFF);
+
+            }
+        }
+        else if(shortMessageWrapper.isNoteOff()){
+            int i=0;
+            int toDelete=9999;
+            for(int cc : macroToRelease){
+                if(shortMessageWrapper.getData1()==cc){
+                    int position = (shortMessageWrapper.getData1() + Variables.macros_offset) % 10;
+
+                    Keyboard keyboard = new Keyboard();
+                    keyboard.type(macros.get(position).getVirtualKey());
+
+                    macros.get(position).setOn(false);
+                    ShortMessageWrapper message = shortMessageWrapper;
+                    message.alterCommand(0x90);
+                    setLed(false, message, 0, Variables.OFF);
+                    toDelete = i;
+                }
+                i++;
+            }
+            if(toDelete!=9999){
+                macroToRelease.remove(toDelete);
             }
         }
         return false;
@@ -103,12 +149,11 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
 
     }
 
-    public void setLed(boolean status, ShortMessageWrapper messageWrapper, int colorOn, int colorOff){
-        if(status){
+    public void setLed(boolean status, ShortMessageWrapper messageWrapper, int colorOn, int colorOff) {
+        if (status) {
             messageWrapper.alterData2(colorOn);
             getService().send(messageWrapper);
-        }
-        else{
+        } else {
             messageWrapper.alterData2(colorOff);
             getService().send(messageWrapper);
         }
--- src/Variables.java
+++ src/Variables.java
@@ -3,26 +3,26 @@ public class Variables {
     public static int playbacks_offset = 6;
 
     public static String[] playbacks = {
-            "64", "X",
-            "65", "C",
-            "66", "V",
-            "67", "B",
-            "68", "N",
-            "69", "M",
+            "64", "x",
+            "65", "c",
+            "66", "v",
+            "67", "b",
+            "68", "n",
+            "69", "m",
             "70", ",",
             "71", ".",
-            "89", "\\",
-            "88", "Z"
+            "88", "z",
+            "89", "\\"
     };
     public static int stops_offset = 0;
     public static String[] stops = {
-            "0", "D",
-            "1", "F",
-            "2", "G",
-            "3", "H",
-            "4", "J",
-            "5", "K",
-            "6", "L",
+            "0", "d",
+            "1", "f",
+            "2", "g",
+            "3", "h",
+            "4", "j",
+            "5", "k",
+            "6", "l",
             "7", ";"
     };
     public static int taps_offset = 2;
@@ -39,14 +39,14 @@ public class Variables {
 
     public static int macros_offset = 4;
     public static String[] macros = {
-            "56", "Q", "TRUE",
-            "57", "W", "TRUE",
-            "58", "E", "TRUE",
-            "59", "R", "TRUE",
-            "60", "T", "TRUE",
-            "61", "Y", "TRUE",
-            "62", "U", "TRUE",
-            "63", "I", "TRUE"
+            "56", "q", "TRUE",
+            "57", "w", "TRUE",
+            "58", "e", "TRUE",
+            "59", "r", "TRUE",
+            "60", "t", "TRUE",
+            "61", "y", "TRUE",
+            "62", "u", "TRUE",
+            "63", "i", "TRUE"
     };
     public static int OFF = 0;
     public static int GREEN = 1;
