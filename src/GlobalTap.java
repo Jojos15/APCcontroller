dc789d872baf199509ed8d1dc4f3e9c6eeb3e5dd
jojos15 <georgekosmidis1@yahoo.gr>
2018-09-05 18:20:37 +0300

    Threaded global tap

---
+++ src/GlobalTap.java
@@ -0,0 +1,70 @@
+import javax.naming.Context;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GlobalTap implements Runnable {
+
+    MainThread mainThread;
+
+    public GlobalTap(MainThread mainThread){
+        this.mainThread = mainThread;
+    }
+
+    @Override
+    public void run() {
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+        ArrayList<Integer> tempList2 = new ArrayList<Integer>();
+        ArrayList<Integer> tempList3 = new ArrayList<Integer>();
+        int white = new Color(255, 255, 255).getRGB();
+        try {
+            Robot robot = new Robot();
+            BufferedImage image = robot.createScreenCapture(new Rectangle(1366, 768));
+            for (int j = 407; j <= 414; j++) {
+                for (int i = 934; i <= 937; i++) {
+                    int color = image.getRGB(i, j);
+                    if (color == white) {
+                        tempList.add(i - 934);
+                        tempList.add(j - 407);
+                    }
+                }
+                for (int i = 940; i <= 943; i++) {
+                    int color = image.getRGB(i, j);
+                    if (color == white) {
+                        tempList2.add(i - 940);
+                        tempList2.add(j - 407);
+                    }
+                }
+                for (int i = 946; i <= 949; i++) {
+                    int color = image.getRGB(i, j);
+                    if (color == white) {
+                        tempList3.add(i - 946);
+                        tempList3.add(j - 407);
+                    }
+                }
+            }
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        Integer[] toCheck = new Integer[tempList.size()];
+        toCheck = tempList.toArray(toCheck);
+        Integer[] toCheck2 = new Integer[tempList2.size()];
+        toCheck2 = tempList2.toArray(toCheck2);
+        Integer[] toCheck3 = new Integer[tempList3.size()];
+        toCheck3 = tempList3.toArray(toCheck3);
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            if (Arrays.equals(toCheck, Variables.digitPixelsWhite[i])) {
+                sum+=i*100;
+            }
+            if (Arrays.equals(toCheck2, Variables.digitPixelsWhite[i])) {
+                sum+=i*10;
+            }
+            if (Arrays.equals(toCheck3, Variables.digitPixelsWhite[i])) {
+                sum+=i;
+            }
+        }
+        mainThread.getService().log(Integer.toString(sum));
+    }
+}
--- src/MainThread.java
+++ src/MainThread.java
@@ -3,6 +3,7 @@ import com.confusionists.mjdjApi.midi.ShortMessageWrapper;
 import com.confusionists.mjdjApi.morph.DeviceNotFoundException;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import javax.naming.Context;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.ShortMessage;
 import javax.swing.*;
@@ -66,7 +67,7 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
             } else
                 playbackPixels.add(new PlaybackPixel(Integer.parseInt(Variables.playbacks[i * 2]), Variables.testPixelsX[i], Variables.testPixelsY));
         }
-
+        // Starting Animation
         /*for (int i = 0; i < 98; i++) {
             try {
                 lights[i] = 0;
@@ -94,61 +95,10 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        long mi = System.currentTimeMillis();
-        ArrayList<Integer> tempList = new ArrayList<Integer>();
-        ArrayList<Integer> tempList2 = new ArrayList<Integer>();
-        ArrayList<Integer> tempList3 = new ArrayList<Integer>();
-        int white = new Color(255, 255, 255).getRGB();
-        try {
-            Robot robot = new Robot();
-            BufferedImage image = robot.createScreenCapture(new Rectangle(1366, 768));
-            for (int j = 407; j <= 414; j++) {
-                for (int i = 934; i <= 937; i++) {
-                    int color = image.getRGB(i, j);
-                    if (color == white) {
-                        tempList.add(i - 934);
-                        tempList.add(j - 407);
-                    }
-                }
-                for (int i = 940; i <= 943; i++) {
-                    int color = image.getRGB(i, j);
-                    if (color == white) {
-                        tempList2.add(i - 940);
-                        tempList2.add(j - 407);
-                    }
-                }
-                for (int i = 946; i <= 949; i++) {
-                    int color = image.getRGB(i, j);
-                    if (color == white) {
-                        tempList3.add(i - 946);
-                        tempList3.add(j - 407);
-                    }
-                }
-            }
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-        System.out.println(System.currentTimeMillis() - mi);
-        Integer[] toCheck = new Integer[tempList.size()];
-        toCheck = tempList.toArray(toCheck);
-        Integer[] toCheck2 = new Integer[tempList2.size()];
-        toCheck2 = tempList2.toArray(toCheck2);
-        Integer[] toCheck3 = new Integer[tempList3.size()];
-        toCheck3 = tempList3.toArray(toCheck3);
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            if (Arrays.equals(toCheck, Variables.digitPixelsWhite[i])) {
-                sum+=i*100;
-            }
-            if (Arrays.equals(toCheck2, Variables.digitPixelsWhite[i])) {
-                sum+=i*10;
-            }
-            if (Arrays.equals(toCheck3, Variables.digitPixelsWhite[i])) {
-                sum+=i;
-            }
-        }
-        System.out.println(sum);
-        System.out.println(System.currentTimeMillis() - mi);
+
+        GlobalTap globalTap = new GlobalTap(this);
+        Thread thread = new Thread(globalTap);
+        thread.start();
     }
 
     @Override
@@ -326,7 +276,7 @@ public class MainThread extends com.confusionists.mjdjApi.morph.AbstractMorph {
 
     public void checkPixels() throws AWTException, InvalidMidiDataException {
         for (PlaybackPixel p : playbackPixels) {
-            getService().log(Boolean.toString(p.checkPixelColor(255, 255, 255)));
+
             if (!(p.checkPixelColor(255, 255, 255)) && lights[p.getPlaybackCC()] > 0) {
                 getService().send(MessageWrapper.newInstance(new ShortMessage(ShortMessage.NOTE_ON, 0, p.getPlaybackCC(), 0)));
                 for (Playback pl : playbacks) {
